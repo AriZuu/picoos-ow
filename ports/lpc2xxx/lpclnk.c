5bbdf02bf4ef630c169ae01cbe144e48d6cf6714
Ari Suutari <ari@stonepile.fi>
2013-09-08 02:36:59 +0000

    OneWire embedded stuff library added.

---
+++ ports/lpc2xxx/lpclnk.c
@@ -0,0 +1,382 @@
+#include <picoos.h>
+#include <picoos-u.h>
+
+//---------------------------------------------------------------------------
+// Copyright (C) 2001 Dallas Semiconductor Corporation, All Rights Reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL DALLAS SEMICONDUCTOR BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// Except as contained in this notice, the name of Dallas Semiconductor
+// shall not be used except as stated in the Dallas Semiconductor
+// Branding Policy.
+//---------------------------------------------------------------------------
+//
+//  TODO.C - Link Layer functions required by general 1-Wire drive
+//           implimentation.  Fill in the platform specific code.
+//
+//  Version: 3.00
+//
+//  History: 1.00 -> 1.01  Added function msDelay.
+//           1.02 -> 1.03  Added function msGettick.
+//           1.03 -> 2.00  Changed 'MLan' to 'ow'. Added support for
+//                         multiple ports.
+//           2.10 -> 3.00  Added owReadBitPower and owWriteBytePower
+//
+
+#include "lpc_reg.h"
+#include <picoos.h>
+#include "picoos-ow.h"
+
+#define OW_BIT (1<<OWCFG_GPIO_PIN)
+
+#define OW_GET_IN()   ( (GPIO0_IOPIN & OW_BIT) ? 1 : 0 )
+#define OW_OUT_LOW()  ( GPIO0_IOCLR = OW_BIT )
+#define OW_OUT_HIGH() ( GPIO0_IOSET = OW_BIT )
+#define OW_DIR_IN()   ( GPIO0_IODIR &= ~(OW_BIT) )
+#define OW_DIR_OUT()  ( GPIO0_IODIR |= OW_BIT )
+
+// exportable link-level functions
+SMALLINT owTouchReset(int);
+SMALLINT owTouchBit(int,SMALLINT);
+SMALLINT owTouchByte(int,SMALLINT);
+SMALLINT owWriteByte(int,SMALLINT);
+SMALLINT owReadByte(int);
+SMALLINT owSpeed(int,SMALLINT);
+SMALLINT owLevel(int,SMALLINT);
+SMALLINT owProgramPulse(int);
+void msDelay(int);
+long msGettick(void);
+SMALLINT hasPowerDelivery(int);
+SMALLINT hasOverDrive(int);
+SMALLINT hasProgramPulse(int);
+SMALLINT owWriteBytePower(int,SMALLINT);
+SMALLINT owReadBitPower(int, SMALLINT);
+
+//--------------------------------------------------------------------------
+// Reset all of the devices on the 1-Wire Net and return the result.
+//
+// 'portnum'    - number 0 to MAX_PORTNUM-1.  This number is provided to
+//                indicate the symbolic port number.
+//
+// Returns: TRUE(1):  presense pulse(s) detected, device(s) reset
+//          FALSE(0): no presense pulses detected
+//
+SMALLINT owTouchReset(int portnum)
+{
+   int err;
+   POS_LOCKFLAGS;
+	
+   POS_SCHED_LOCK;
+
+// pull OW-Pin low for 480us
+
+   OW_OUT_LOW();
+   OW_DIR_OUT();
+	
+   uosSpinUSecs(480);
+	
+// set Pin as input - wait for clients to pull low
+
+   OW_DIR_IN();
+	
+   uosSpinUSecs(70);
+   err = OW_GET_IN();
+	
+// after a delay the clients should release the line
+// and input-pin gets back to high due to pull-up-resistor
+
+   uosSpinUSecs(480);
+   if( OW_GET_IN() == 0 )		// short circuit
+      err = 1;
+	
+   POS_SCHED_UNLOCK;
+   if (err)
+      return FALSE;
+		
+   return TRUE;
+}
+
+//--------------------------------------------------------------------------
+// Send 1 bit of communication to the 1-Wire Net and return the
+// result 1 bit read from the 1-Wire Net.  The parameter 'sendbit'
+// least significant bit is used and the least significant bit
+// of the result is the return bit.
+//
+// 'portnum'    - number 0 to MAX_PORTNUM-1.  This number is provided to
+//                indicate the symbolic port number.
+// 'sendbit'    - the least significant bit is the bit to send
+//
+// Returns: 0:   0 bit read from sendbit
+//          1:   1 bit read from sendbit
+//
+SMALLINT owTouchBit(int portnum, SMALLINT sendbit)
+{
+   POS_LOCKFLAGS;
+
+   POS_SCHED_LOCK;
+
+// drive bus low
+
+   OW_DIR_OUT();
+   uosSpinUSecs(1);
+
+// if bit is 1 set bus high (by ext. pull-up)
+
+   if (sendbit)
+      OW_DIR_IN();
+		
+   uosSpinUSecs(15-1);
+
+// sample at end of read-timeslot
+
+   if (OW_GET_IN() == 0)
+      sendbit = 0;
+	
+   uosSpinUSecs(60-15);
+   OW_DIR_IN();
+	
+   POS_SCHED_UNLOCK;
+   return sendbit;
+}
+
+//--------------------------------------------------------------------------
+// Send 8 bits of communication to the 1-Wire Net and return the
+// result 8 bits read from the 1-Wire Net.  The parameter 'sendbyte'
+// least significant 8 bits are used and the least significant 8 bits
+// of the result is the return byte.
+//
+// 'portnum'    - number 0 to MAX_PORTNUM-1.  This number is provided to
+//                indicate the symbolic port number.
+// 'sendbyte'   - 8 bits to send (least significant byte)
+//
+// Returns:  8 bytes read from sendbyte
+//
+SMALLINT owTouchByte(int portnum, SMALLINT sendbyte)
+{
+   int i;
+   unsigned result = 0u;
+
+   for (i = 0; i < 8; i++){
+
+      if (owTouchBit(portnum,sendbyte & 1) != 0)
+         result |= (1u << i);
+       
+      sendbyte >>= 1;
+   }
+
+   return (SMALLINT)result;
+}
+
+//--------------------------------------------------------------------------
+// Send 8 bits of communication to the 1-Wire Net and verify that the
+// 8 bits read from the 1-Wire Net is the same (write operation).
+// The parameter 'sendbyte' least significant 8 bits are used.
+//
+// 'portnum'    - number 0 to MAX_PORTNUM-1.  This number is provided to
+//                indicate the symbolic port number.
+// 'sendbyte'   - 8 bits to send (least significant byte)
+//
+// Returns:  TRUE: bytes written and echo was the same
+//           FALSE: echo was not the same
+//
+SMALLINT owWriteByte(int portnum, SMALLINT sendbyte)
+{
+   return (owTouchByte(portnum,sendbyte) == sendbyte) ? TRUE : FALSE;
+}
+
+//--------------------------------------------------------------------------
+// Send 8 bits of read communication to the 1-Wire Net and and return the
+// result 8 bits read from the 1-Wire Net.
+//
+// 'portnum'    - number 0 to MAX_PORTNUM-1.  This number is provided to
+//                indicate the symbolic port number.
+//
+// Returns:  8 bytes read from 1-Wire Net
+//
+SMALLINT owReadByte(int portnum)
+{
+   return owTouchByte(portnum,0xFF);
+}
+
+//--------------------------------------------------------------------------
+// Set the 1-Wire Net communucation speed.
+//
+// 'portnum'    - number 0 to MAX_PORTNUM-1.  This number is provided to
+//                indicate the symbolic port number.
+// 'new_speed'  - new speed defined as
+//                MODE_NORMAL     0x00
+//                MODE_OVERDRIVE  0x01
+//
+// Returns:  current 1-Wire Net speed
+//
+SMALLINT owSpeed(int portnum, SMALLINT new_speed)
+{
+
+// not supported always return normal
+
+   return MODE_NORMAL;
+}
+
+//--------------------------------------------------------------------------
+// Set the 1-Wire Net line level.  The values for NewLevel are
+// as follows:
+//
+// 'portnum'    - number 0 to MAX_PORTNUM-1.  This number is provided to
+//                indicate the symbolic port number.
+// 'new_level'  - new level defined as
+//                MODE_NORMAL     0x00
+//                MODE_STRONG5    0x02
+//                MODE_PROGRAM    0x04
+//                MODE_BREAK      0x08
+//
+// Returns:  current 1-Wire Net level
+//
+SMALLINT owLevel(int portnum, SMALLINT new_level)
+{
+   if (new_level == MODE_NORMAL ||
+       new_level == MODE_STRONG5)
+      return new_level;
+
+   return MODE_NORMAL;
+}
+
+//--------------------------------------------------------------------------
+// This procedure creates a fixed 480 microseconds 12 volt pulse
+// on the 1-Wire Net for programming EPROM iButtons.
+//
+// 'portnum'    - number 0 to MAX_PORTNUM-1.  This number is provided to
+//                indicate the symbolic port number.
+//
+// Returns:  TRUE  successful
+//           FALSE program voltage not available
+//
+SMALLINT owProgramPulse(int portnum)
+{
+   return 0;
+}
+
+//--------------------------------------------------------------------------
+//  Description:
+//     Delay for at least 'len' ms
+//
+void msDelay(int len)
+{
+   posTaskSleep(MS(len)); // XXX use HZ 
+}
+
+#if 0
+//--------------------------------------------------------------------------
+// Get the current millisecond tick count.  Does not have to represent
+// an actual time, it just needs to be an incrementing timer.
+//
+long msGettick(void)
+{
+   // add platform specific code here
+   return 0;
+}
+#endif
+
+//--------------------------------------------------------------------------
+// Send 8 bits of communication to the 1-Wire Net and verify that the
+// 8 bits read from the 1-Wire Net is the same (write operation).  
+// The parameter 'sendbyte' least significant 8 bits are used.  After the
+// 8 bits are sent change the level of the 1-Wire net.
+//
+// 'portnum'  - number 0 to MAX_PORTNUM-1.  This number was provided to
+//              OpenCOM to indicate the port number.
+// 'sendbyte' - 8 bits to send (least significant byte)
+//
+// Returns:  TRUE: bytes written and echo was the same
+//           FALSE: echo was not the same 
+//
+SMALLINT owWriteBytePower(int portnum, SMALLINT sendbyte)
+{
+   if(owTouchByte(portnum,sendbyte) != sendbyte)
+      return FALSE;
+
+   if(owLevel(portnum,MODE_STRONG5) != MODE_STRONG5)
+      return FALSE;
+
+   return TRUE;
+}
+
+//--------------------------------------------------------------------------
+// Send 1 bit of communication to the 1-Wire Net and verify that the
+// response matches the 'applyPowerResponse' bit and apply power delivery
+// to the 1-Wire net.  Note that some implementations may apply the power
+// first and then turn it off if the response is incorrect.
+//
+// 'portnum'  - number 0 to MAX_PORTNUM-1.  This number was provided to
+//              OpenCOM to indicate the port number.
+// 'applyPowerResponse' - 1 bit response to check, if correct then start
+//                        power delivery 
+//
+// Returns:  TRUE: bit written and response correct, strong pullup now on
+//           FALSE: response incorrect
+//
+SMALLINT owReadBitPower(int portnum, SMALLINT applyPowerResponse)
+{
+   if(owTouchBit(portnum,0x01) != applyPowerResponse)
+      return FALSE;
+
+   if(owLevel(portnum,MODE_STRONG5) != MODE_STRONG5)
+      return FALSE;
+
+   return TRUE;
+}
+
+//--------------------------------------------------------------------------
+// This procedure indicates wether the adapter can deliver power.
+//
+// 'portnum'  - number 0 to MAX_PORTNUM-1.  This number was provided to
+//              OpenCOM to indicate the port number.
+//
+// Returns:  TRUE  if adapter is capable of delivering power. 
+//
+SMALLINT hasPowerDelivery(int portnum)
+{
+   return TRUE;
+}
+
+//--------------------------------------------------------------------------
+// This procedure indicates wether the adapter can deliver power.
+//
+// 'portnum'  - number 0 to MAX_PORTNUM-1.  This number was provided to
+//              OpenCOM to indicate the port number.
+//
+// Returns:  TRUE  if adapter is capable of over drive. 
+//
+SMALLINT hasOverDrive(int portnum)
+{
+   return TRUE;
+}
+//--------------------------------------------------------------------------
+// This procedure creates a fixed 480 microseconds 12 volt pulse 
+// on the 1-Wire Net for programming EPROM iButtons.
+//
+// 'portnum'  - number 0 to MAX_PORTNUM-1.  This number was provided to
+//              OpenCOM to indicate the port number.
+//
+// Returns:  TRUE  program volatage available
+//           FALSE program voltage not available  
+SMALLINT hasProgramPulse(int portnum)
+{
+   return FALSE;
+}
+
